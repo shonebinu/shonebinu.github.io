a76535d0dd5247ada323afc81b9697f9fde7963c
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-07-28 22:38:08 +0530

    setup github deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,16 +4,15 @@ import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import { SITE_URL } from "./src/data/config";
 
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
-  site: SITE_URL,
-  markdown: {
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "nord",
-      wrap: false
-    }
-  }
+	integrations: [tailwind(), sitemap(), robotsTxt()],
+	site: SITE_URL,
+	markdown: {
+		syntaxHighlight: "shiki",
+		shikiConfig: {
+			theme: "nord",
+			wrap: false,
+		},
+	},
 });
--- src/data/config.ts
+++ src/data/config.ts
@@ -1,1 +1,1 @@
-export const SITE_URL = "https://shonebinu.com/";
+export const SITE_URL = "https://www.shonebinu.com/";
